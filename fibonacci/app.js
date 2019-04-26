7ab8b33c5e135c1b3b867deb4937658c01ffbb4b
ing_mario <ing_mariomcs@outlook.com>
2019-04-26 17:53:57 -0500

    libreria yargs para comandos

--- fibonacci/app.js
+++ fibonacci/app.js
@@ -1,10 +1,18 @@
+const argv = require('yargs').command('fibonacci', 'dame un numero', {
+  base: {
+    demand:true,
+    alias: 'n'
+  }
+}).help().argv;
+
+let number = argv.base;
 let a = [];
 let res = [];
 a[0] = 0;
 a[1] = 1;
 
-for (let x = 2; x < 30; x++) {
-    let res = a[x-2] + a[x-1];
-    a[x] = res;
+for (let x = 2; x < number; x++) {
+  let res = a[x - 2] + a[x - 1];
+  a[x] = res;
 }
-    console.log(a);
+console.log(a);
--- primos/app.js
+++ primos/app.js
@@ -1,18 +1,27 @@
-let num = 29;
+const argv = require('yargs').command('primos', 'dame numero', {
+    base: {
+      demand: true,
+      alias: 'n'
+    }
+  })
+  .help().argv;
+
+let num = argv.base;
 
 if (num == 1 || num == 2) {
   console.log(`El numero ${num} es primo`);
 }
-
 if (num == 1 || num == 2) {
-    resp: 'El numero ' + num + ' es primo'
-} else {
+  resp: 'El numero ' + num + ' es primo'
+}
+else {
   for (var i = 2; i < num; i++) {
     if (num % i == 0) {
       console.log(`El numero ${num} no es primo`);
-    }else {
-    console.log(`El numero ${num} es primo`);
-    break;
-  }
+      break;
+    } else {
+      console.log(`El numero ${num} es primo`);
+      break;
+    }
   }
 }
